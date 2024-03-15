858907a7bda4dabd913a26f69e99227885bffee8
Papayadev2024 <diegomartinez.papaya@gmail.com>
2024-03-15 17:28:54 -0500

    cambio de txt

--- resources/views/Homepage.blade.php
+++ resources/views/Homepage.blade.php
@@ -78,7 +78,7 @@
          
           <div class="items-center  xs:pt-[5%] md:pt-[5%] lg:pt-[5%]  xs:px-[2%] md:px-[8%] ">
             <h1 class="xs:text-4xl lg:text-5xl   text-white w-full font-bold font-RightgroteskMedium tracking-wider">
-              ¡Mundo Web,<br> Nuestra pasión por la transformación digital!
+               ¡Nuestra pasión por la transformación digital!
             </h1>
             <p class="xs:text-base xl:text-base text-white  font-normal mt-6 font-MontserratRegular ">Somos una empresa de desarrollo web joven y dinámica, especializada en transformar ideas en 
               soluciones digitales excepcionales. En Mundo Web, no solo construimos sitios web y aplicativos, 
