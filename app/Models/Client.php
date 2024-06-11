6d2ee661ff01050d4e1b578942da1045cbe59e4b
Papayadev2024 <diegomartinez.papaya@gmail.com>
2024-06-11 11:50:38 -0500

    correccion

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -12,7 +12,7 @@ class Client extends Model
     protected $connection = "mysql_atalaya";
 
     protected $fillable=['ruc','name', 'description', 'contact_name', 'contact_phone' , 'contact_email', 'contact_address' ,'contact_position', 'message' ,
-                        'web_url', 'source', 'fecha', 'hora', 'ip', 'origin', 'client_width', 'client_height', 'client_latitude', 'client_longitude', 
+                        'web_url', 'source', 'date', 'time', 'ip', 'origin', 'client_width', 'client_height', 'client_latitude', 'client_longitude', 
                         'client_system', 'status_id'
                         ];
 }
