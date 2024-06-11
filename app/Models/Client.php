2b1124c6700a1c1a716f5cf0f21ed02afc7fdbe4
Papayadev2024 <diegomartinez.papaya@gmail.com>
2024-06-11 11:46:49 -0500

    correccion

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -9,7 +9,7 @@ class Client extends Model
 {
     use HasFactory;
 
-    protected $conection = "mysql_atalaya";
+    protected $connection = "mysql_atalaya";
 
     protected $fillable=['ruc','name', 'description', 'contact_name', 'contact_phone' , 'contact_email', 'contact_address' ,'contact_position', 'message' ,
                         'web_url', 'source', 'fecha', 'hora', 'ip', 'origin', 'client_width', 'client_height', 'client_latitude', 'client_longitude', 
