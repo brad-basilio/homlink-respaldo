f05339065fd3126948592ecb123eae19d8e26db3
Papayadev2024 <diegomartinez.papaya@gmail.com>
2024-06-12 15:16:04 -0500

    final

--- routes/web.php
+++ routes/web.php
@@ -58,3 +58,8 @@ Route::post('/landingaplicativos/form', [LandingController::class, 'storeAplicat
 Route::post('/landingmundoweb/form', [LandingController::class, 'storeLanding'] )->name('guardarlandingmundoweb');
 Route::post('/landingecommerce/form', [LandingController::class, 'storeEcommerce'] )->name('guardarlandingecommerce');
 Route::post('/landingwebsite/form', [LandingController::class, 'storeWebsite'] )->name('guardarlandingwebsite');
+
+
+Route::domain('websites.mundoweb.pe')->group(function () {
+    Route::get('/', [LandingController::class, 'viewLandingpagemundoweb'])->name('ultimalanding');
+});
