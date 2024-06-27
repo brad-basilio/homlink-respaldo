418fbe4b6c87eac3022b6e6f1169ba1bb09b1628
Papayadev2024 <diegomartinez.papaya@gmail.com>
2024-06-27 09:27:19 -0500

    final

--- app/Helpers/EmailConfig.php
+++ app/Helpers/EmailConfig.php
@@ -14,8 +14,10 @@ class EmailConfig
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'hola@mundoweb.pe';
-        $mail->Password = 'uohombtocndviqyz';
+        // $mail->Username = 'hola@mundoweb.pe';
+        // $mail->Password = 'uohombtocndviqyz';
+        $mail->Username = 'diegomartinez1996x@gmail.com';
+        $mail->Password = 'piqutsbcfdzsrnkp';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
         $mail->Port = 465;
         $mail->Subject = '' . $name . ', Gracias por comunicarte con Mundo Web';
