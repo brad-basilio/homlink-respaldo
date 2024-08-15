b29ff13ed9c17c21d5310676e54ed43b55d46134
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-08-15 10:01:06 -0500

    NTS

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -21,7 +21,6 @@ class FormController extends Controller
        
         $response = Response::simpleTryCatch(function (Response $response) use ($request) {
             $body = $request->all();
-            dd($body);
             $body['origin'] = $body['origin'] ?? '[Mundo Web] - Landing WebSite';
             $body['source'] = $body['source'] ?? 'Integracion API';
             $body['triggered_by'] = $body['triggered_by'] ?? 'Pauta';
