e6e8f9c4a87168f348027f12e161007dbd4af024
sodeworld <160994235+sodeworld@users.noreply.github.com>
2024-09-27 11:42:50 -0500

    Hola mundo

--- resources/js/BlogArticle.jsx
+++ resources/js/BlogArticle.jsx
@@ -73,7 +73,7 @@ const BlogArticle = ({ previousArticle, article, nextArticle }) => {
               <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20">
                 <path fillRule="evenodd" d="M17.707 9.293a1 1 0 010 1.414l-7 7a1 1 0 01-1.414 0l-7-7A.997.997 0 012 10V5a3 3 0 013-3h5c.256 0 .512.098.707.293l7 7zM5 6a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
               </svg>
-              {article.tags?.filter(x => x.visible && x.status).map(x => <span>{x.name.trim()}</span>)}
+              {article.tags?.filter(x => x.visible && x.status).map(x => <span className='px-2 me-1 rounded-md bg-green-200'>{x.name.trim()}</span>)}
             </div>
             <div className="flex items-center">
               <span className="mr-2">Compartir esta publicación</span>
