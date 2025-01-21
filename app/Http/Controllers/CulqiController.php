bd9cf4ce47e03ca41553d2e057c65962512a5220
gysmoy <gamboapalominocarlosmanuel@gmail.com>
2025-01-21 09:50:21 -0500

    Agregando venta a webhook

--- app/Http/Controllers/CulqiController.php
+++ app/Http/Controllers/CulqiController.php
@@ -313,10 +313,13 @@ class CulqiController extends Controller
 
       if ($data['state'] != 'paid') return;
 
-      $sale = Sale::select(['id', 'status_id'])
+      Sale::select(['id', 'status_id'])
         ->where('code', $code)
         ->update(['status_id' => '312f9a91-d3f2-4672-a6bf-678967616cac']);
 
+      $sale = Sale::where('code', $code)->first();
+      $sale->status_id = '312f9a91-d3f2-4672-a6bf-678967616cac';
+      $sale->save();
       SendSaleWhatsApp::dispatchAfterResponse($sale);
     });
 
