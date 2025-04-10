434366a9eb44739213f71e15723f443907bac898
brad-basilio <brad.basilioppy@gmail.com>
2025-04-09 20:35:21 -0500

    lg

--- resources/js/components/Blog/Results.jsx
+++ resources/js/components/Blog/Results.jsx
@@ -1,54 +1,58 @@
-import React, { useEffect, useState } from "react"
-import FilterPagination from "../../Reutilizables/Pagination/FilterPagination"
-import PostCard from "./PostCard"
-import PostsRest from "../../Actions/PostsRest"
-import ArrayJoin from "../../Utils/ArrayJoin"
+import React, { useEffect, useState } from "react";
+import FilterPagination from "../../Reutilizables/Pagination/FilterPagination";
+import PostCard from "./PostCard";
+import PostsRest from "../../Actions/PostsRest";
+import ArrayJoin from "../../Utils/ArrayJoin";
 
-const postsRest = new PostsRest()
+const postsRest = new PostsRest();
 
 const Results = ({ filter }) => {
-  const [results, setResults] = useState([])
-  const [pages, setPages] = useState(1);
-  const [currentPage, setCurrentPage] = useState(1);
+    const [results, setResults] = useState([]);
+    const [pages, setPages] = useState(1);
+    const [currentPage, setCurrentPage] = useState(1);
 
-  useEffect(() => {
-    const filter2search = [
-      ['name', 'contains', filter.search],
-      ['summary', 'contains', filter.search],
-    ];
-    if (filter.category) {
-      filter2search.push([
-        'category.id', '=', filter.category
-      ])
-    }
+    useEffect(() => {
+        const filter2search = [
+            ["name", "contains", filter.search],
+            ["summary", "contains", filter.search],
+        ];
+        if (filter.category) {
+            filter2search.push(["category.id", "=", filter.category]);
+        }
 
-    postsRest.paginate({
-      filter: ArrayJoin(filter2search, 'and'),
-      requireTotalCount: true,
-      skip: 6 * (currentPage - 1),
-      sort: [{ selector: 'post_date', desc: filter.sortOrder == 'desc' }],
-      take: 6,
-    })
-      .then(({ status, data, totalCount }) => {
-        if (status != 200) return
-        setPages(Math.ceil(totalCount / 12))
-        setResults(data)
-      })
+        postsRest
+            .paginate({
+                filter: ArrayJoin(filter2search, "and"),
+                requireTotalCount: true,
+                skip: 6 * (currentPage - 1),
+                sort: [
+                    { selector: "post_date", desc: filter.sortOrder == "desc" },
+                ],
+                take: 6,
+            })
+            .then(({ status, data, totalCount }) => {
+                if (status != 200) return;
+                setPages(Math.ceil(totalCount / 12));
+                setResults(data);
+            });
+    }, [filter, currentPage]);
 
-  }, [filter, currentPage])
+    return (
+        <>
+            <section className="p-[5%] pt-0 grid md:grid-cols-2 lg:grid-cols-3  gap-8 lg:px-0 lg:max-w-[82rem] 2xl:max-w-[92rem] mx-auto">
+                {results.map((item, index) => {
+                    return <PostCard key={index} {...item} firstImage />;
+                })}
+            </section>
+            <div className="p-[5%]">
+                <FilterPagination
+                    pages={pages}
+                    current={currentPage}
+                    setCurrent={setCurrentPage}
+                />
+            </div>
+        </>
+    );
+};
 
-  return <>
-    <section className="p-[5%] pt-0 grid md:grid-cols-2 lg:grid-cols-3  gap-8 lg:px-0 max-w-7xl mx-auto">
-      {
-        results.map((item, index) => {
-          return <PostCard key={index} {...item} firstImage />
-        })
-      }
-    </section>
-    <div className="p-[5%]">
-      <FilterPagination pages={pages} current={currentPage} setCurrent={setCurrentPage} />
-    </div>
-  </>
-}
-
-export default Results
+export default Results;
