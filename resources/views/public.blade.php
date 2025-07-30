ea35e10e5eb941b2f16bafee52f3bd09f1736154
brad-basilio <brad.basilioppy@gmail.com>
2025-07-30 15:19:28 -0500

    primera subida

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -97,35 +97,8 @@
         }
     </style>
 
-    <!-- Meta Pixel Code -->
-    <script>
-        ! function(f, b, e, v, n, t, s) {
-            if (f.fbq) return;
-            n = f.fbq = function() {
-                n.callMethod ?
-                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-            };
-            if (!f._fbq) f._fbq = n;
-            n.push = n;
-            n.loaded = !0;
-            n.version = '2.0';
-            n.queue = [];
-            t = b.createElement(e);
-            t.async = !0;
-            t.src = v;
-            s = b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t, s)
-        }(window, document, 'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '1098274404490481');
-        fbq('track', 'PageView');
-    </script>
-    <noscript>
-        <img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id=1098274404490481&ev=PageView&noscript=1" />
-    </noscript>
-    <!-- End Meta Pixel Code -->
-    <link rel="stylesheet" href="/assets/fonts/aspekta/font-face.css" />
+ 
+
 </head>
 
 <style>
@@ -177,20 +150,7 @@
         });
     </script>
 
-    <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
-        var Tawk_API=Tawk_API||{};
-        Tawk_LoadStart=new Date();
-        (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/5c3563c212db2461b16b4a9b/default';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-        })();
-    </script>
-    <!--End of Tawk.to Script-->
+  
 
     <!-- PWA Service Worker Registration -->
     <script>
