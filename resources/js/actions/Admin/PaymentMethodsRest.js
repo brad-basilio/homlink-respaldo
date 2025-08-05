e4e5df481023a694bb4bac410b7cf2f7022eb584
brad-basilio <brad.basilioppy@gmail.com>
2025-08-05 18:28:40 -0500

    new vuild

--- resources/js/actions/Admin/PaymentMethodsRest.js
+++ resources/js/actions/Admin/PaymentMethodsRest.js
@@ -1,4 +1,4 @@
-import BasicRest from "../../Actions/BasicRest";
+import BasicRest from "../../actions/BasicRest";
 
 class PaymentMethodsRest extends BasicRest {
     path = 'admin/payment-methods'
